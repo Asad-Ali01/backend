7cfd484d1ec894fc2e8b8a3384e7f8364a136e46
Asad Ali <mail.asadali01@gmail.com>
2026-01-10 21:15:28 +0500

    Make some new files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import { DB_NAME } from "./constants.js";
 import connectDB from "./db/dbConnection.js";
 import express from 'express';
 import dotenv from 'dotenv'
-dotenv.config({path:'./.env'})
-await connectDB()
+import { app } from "./app.js";
+dotenv.config(
+    {path:'./.env'}
+)
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000,() => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+    app.on("error",(err) => {
+        console.log("App error: ",err);
+        throw err;
+    })
+})
+.catch((err) => {
+    console.log('MongoDB connection failed !!!',err);
+})
 
 
 
@@ -45,27 +62,3 @@ await connectDB()
 
 
 
-/*
-
-const app = express();
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log("db is connected");
-        mongoose.connection.on("error",() => {
-            console.log("App failed to talk with db");
-            process.exit(1);   //terminates the nodejs global object which stop app
-        })
-        const server = app.listen(process.env.PORT,()=>{
-            console.log('Server is running');
-        })
-        server.on('error',()=>{
-            console.log("server failed to start");
-            process.exit(1);
-        })
-    } catch (error) {
-        console.error("ERROR:", error );
-        process.exit(1);
-    }
-})()
-    */
